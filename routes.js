80b656ed3319569c4f3e3fc418fdc9e854f18b19
LaupWing <loc_strict@hotmail.com>
2019-07-15 23:48:19 +0200

    update

--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ router.post('/flatIcon', async (req,res)=>{
 
 router.post('/google', async (req,res)=>{
     let searchTerm = req.body.search
-    const result     = await getIconFromGoogle(searchTerm)
+    const result   = await getIconFromGoogle(searchTerm)
     res.send(result)
 })
 
@@ -47,25 +47,55 @@ async function getIconFromFlaticon(searchTerm){
 }
 
 async function getIconFromGoogle(searchterm){
-    const evalVal = searchTerm
+    const evalVal = searchterm
     const browser = await puppeteer.launch({devtools: true})
     const page = await browser.newPage()
     await page.goto('https://www.google.com/')
     await page.waitFor('input[type="text"]')
     await page.evaluate((evalVal)=>{
-        document.querySelector('input[type="text"]').value = evalVal
+        document.querySelector('input[type="text"]').value = evalVal + "icon.png"
     }, evalVal)
     await page.evaluate(() => {
-        document.querySelectorAll('button[type="submit"].flaticon-magnifier.nostyle')[1].click();
+        const inputs = Array.from(document.querySelectorAll('input[type="submit"]'))
+        const filterout = inputs.filter(input=>{
+            if(input.value.trim()==="Google zoeken")    return input
+        })
+        filterout[1].click()
+    })
+    await page.waitForNavigation()
+    await page.evaluate(() => {
+        const a = Array.from(document.querySelectorAll('#top_nav a'))
+        a.forEach(link=>{
+            if(link.innerText==="Afbeeldingen") link.click()
+        })
     })
     await page.waitForNavigation()
     const imgLinks = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('ul.icons li.icon img'))
-            .map(item=>item.src)
-
+        String.prototype.replaceAll = function(search, replacement) {
+            var target = this;
+            return target.replace(new RegExp(search, 'g'), replacement);
+        };
+        const imgs = Array.from(document.querySelectorAll('#search a'))
+            .filter(y=>{
+                if(y.querySelector('img')!==null)   return y
+            })
+            .filter(x=>{
+                if(x.querySelector('img').src !=="") return x
+            })
+            .slice(0,20)
+            .map(a=>a.href)
+            .map(a=>{
+                return a.split("https://www.google.com/imgres?imgurl=")[1]
+                        .split("&imgrefurl=")[0]
+                        .replaceAll("%2F", "/")
+                        .replaceAll("%3A", ":")
+            })
+        console.log(imgs)
+        return imgs
     })
     await browser.close()
     return imgLinks
 }
+console.log('logeed die wel')
 
 module.exports = router
